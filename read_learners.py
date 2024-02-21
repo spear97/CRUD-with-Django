997af89d7d54c259269934ae96a8901cd552ef6c
spear97 <72108425+spear97@users.noreply.github.com>
2024-02-21 14:45:46 -0600

    Update read_learners.py

--- read_learners.py
+++ read_learners.py
@@ -3,6 +3,7 @@ import inspect
 import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
 from django.db import connection
+
 # Ensure settings are read
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
@@ -10,13 +11,13 @@ application = get_wsgi_application()
 from crud.models import *
 from datetime import date
 
-
- # Find students with last name "Smith"
-learners_smith = Learner.objects.filter(#<HINT> add last_name check)
+# Find students with last name "Smith"
+learners_smith = Learner.objects.filter(last_name='Smith')  # filter by last name 'Smith'
 print("1. Find learners with last name `Smith`")
 print(learners_smith)
 print("\n")
+
 # Order by dob descending, and select the first two objects
-learners = Learner.objects.order_by(#<HINT> add dob with - as descending )[#<HINT> add index 0:2]
+learners = Learner.objects.order_by('-dob')[:2]  # order by dob descending, select first two
 print("2. Find top two youngest learners")
 print(learners)
