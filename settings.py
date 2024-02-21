4bb5dca25b243911c3b582ec59a7cf986bf89016
spear97 <72108425+spear97@users.noreply.github.com>
2024-02-21 14:45:07 -0600

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,17 +1,19 @@
-# PostgreSQL
+# PostgreSQL Database Configuration
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'PASSWORD': 'Mjk2MjUtc2hwZWFy',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',  # Database engine
+        'NAME': 'postgres',  # Database name
+        'USER': 'postgres',  # Database user
+        'PASSWORD': 'Mjk2MjUtc2hwZWFy',  # Database password
+        'HOST': 'localhost',  # Database host
+        'PORT': '5432',  # Database port
     }
 }
 
+# Installed Apps for Django
 INSTALLED_APPS = (
-    'crud',
+    'crud',  # Installed app 'crud'
 )
 
+# Secret key for this Django project
 SECRET_KEY = 'SECRET KEY for this Django Project'
